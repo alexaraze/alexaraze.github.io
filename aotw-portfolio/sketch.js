98d6ad867f8c2dc21a113a9aeb3b83ca95f94b5b
alexaraze <129803541+alexaraze@users.noreply.github.com>
2023-12-21 22:33:16 -0500

    Add files via upload

---
+++ aotw-portfolio/sketch.js
@@ -0,0 +1,98 @@
+// function toggleProjectSize(element, projectId) {
+//     element.classList.toggle('expanded');
+
+//     if (element.classList.contains('expanded')) {
+//         const expandedContent = getProjectContent(projectId);
+
+//         element.querySelector('.expanded-content').innerHTML = expandedContent;
+//     } else {
+//         element.querySelector('.expanded-content').innerHTML = '';
+//     }
+// }
+
+// function getProjectContent(projectId) {
+//     switch (projectId) {
+//         case 'iSeeYou':
+//             return `
+//                 <h4>I See You</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://codepen.io/alexaraze/pen/mdaOoVj">View</a>
+//             `;
+//         case 'assemblage':
+//             return `
+//                 <h4>Assemblage</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://codepen.io/alexaraze/pen/NWejGzo">View</a>
+//             `;
+//         case 'theMediumIsTheMessage':
+//             return `
+//                 <h4>The Medium is the Message</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                
+//                 </p>
+//                 <a href="https://alexaraze.github.io/aotw-s1-fp/">View</a>
+//             `;
+//         case 'insaniquarium':
+//             return `
+//                 <h4>Insaniquarium</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://alexaraze.github.io/aotw-s2-a2/">View</a>
+//             `;
+//         case 'skyspace':
+//             return `
+//                 <h4>Skyspace</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://editor.p5js.org/alexaraze/sketches/Lv_eCPOTN">View</a>
+//             `;
+//         case 'whiteRabbit':
+//             return `
+//                 <h4>White Rabbit</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://yaxlei.github.io/S3A1/index.html">View</a>
+//             `;
+//         case 'sunshine':
+//             return `
+//                 <h4>Sunshine</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                   
+//                 </p>
+//                 <a href="http://m-ys.github.io/3-2/">View</a>
+//             `;        
+//         case 'flubber':
+//             return `
+//                 <h4>Flubber</h4>
+//                 <img src="flubber-image.jpg" alt="Flubber Project Image">
+//                 <p>
+                    
+//                 </p>
+//                 <a href="https://alexaraze.github.io/flubber/">View</a>
+//             `;
+//         case 'mutterings':
+//             return `
+//                 <h4>Mutterings</h4>
+//                 <p>
+                    
+//                 </p>
+//                 <a href="mutterings.html">View readings</a>
+//             `;
+//         default:
+//             return '';
+//     }
+// }
